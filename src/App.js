76cc912bfca5ece0b71746ba0af409833591df0d
MohithSuresh <mohithsuresh77@gmail.com>
2023-05-19 19:04:41 +0530

    added employee selection functionality

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Header from "./Header";
 import Footer from "./Footer";
+import Employees from "./Employees";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function App() {
   return (
     <>
       <Header></Header>
+      <Employees></Employees>
       <Footer></Footer>
     </>
   );
--- src/Employees.js
+++ src/Employees.js
@@ -1,9 +1,10 @@
 import { useState } from "react";
 import femaleProfile from "./images/femaleProfile.jpg";
 import maleProfile from "./images/maleProfile.jpg";
+import "./App.css";
 
 const Employees = () => {
-  [employees, setEmployees] = useState([
+  const [employees, setEmployees] = useState([
     {
       id: 1,
       fullName: "Bob Jones",
@@ -90,9 +91,92 @@ const Employees = () => {
     },
   ]);
 
+  const [selectedTeam, setSelectedTeam] = useState("TeamA");
+
+  const handleEmployeeCardClick = (e) => {
+    let transformedEmployees = employees.map((employee) => {
+      if (employee.id === parseInt(e.currentTarget.id)) {
+        if (employee.teamName === selectedTeam) {
+          return { ...employee, teamName: "" };
+        } else {
+          return { ...employee, teamName: selectedTeam };
+        }
+      } else {
+        return employee;
+      }
+    });
+    setEmployees(transformedEmployees);
+  };
+
+  const handleEmployeeCardColor = (employee) => {
+    // return employee.teamName === selectedTeam
+    //   ? "card text-white bg-success mb-3"
+    //   : "card text-white bg-primary mb-3";
+
+    if (employee.teamName === selectedTeam) {
+      return "card text-black bg-success mb-3";
+    } else {
+      return "card text-black bg-white mb-3";
+    }
+  };
+
   return (
     <>
-      <main></main>
+      <main>
+        <div className="container">
+          <div className="row justify-content-center mt-3 mb-3">
+            <div className="col-2">
+              <select
+                className="form-select form-select-lg"
+                value={selectedTeam}
+                onChange={(e) => setSelectedTeam(e.target.value)}
+              >
+                <option value="TeamA">TeamA</option>
+                <option value="TeamB">TeamB</option>
+                <option value="TeamC">TeamC</option>
+                <option value="TeamD">TeamD</option>
+              </select>
+            </div>
+          </div>
+          <div className="row justify-content-center mt-3 mb-3">
+            <div className="col-8">
+              <div className="card-collection">
+                {employees.map((employee) => (
+                  <div
+                    className={handleEmployeeCardColor(employee)}
+                    id={employee.id}
+                    style={{ cursor: "pointer" }}
+                    onClick={handleEmployeeCardClick}
+                  >
+                    {employee.gender === "female" ? (
+                      <img
+                        src={femaleProfile}
+                        className="card-img-top"
+                        alt="..."
+                      />
+                    ) : (
+                      <img
+                        src={maleProfile}
+                        className="card-img-top"
+                        alt="..."
+                      />
+                    )}
+
+                    <div className="card-body">
+                      <h5 className="card-title">
+                        Full Name: {employee.fullName}
+                      </h5>
+                      <p className="card-text">
+                        <b>Designation:</b> {employee.designation}
+                      </p>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
     </>
   );
 };
