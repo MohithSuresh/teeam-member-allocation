0697aefc04e8bf887c3a6b6bd446b9997e5a1524
MohithSuresh <mohithsuresh77@gmail.com>
2023-05-19 17:14:24 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Header from "./Header";
+import Footer from "./Footer";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+export default function App() {
+  return (
+    <>
+      <Header></Header>
+      <Footer></Footer>
+    </>
+  );
+}
